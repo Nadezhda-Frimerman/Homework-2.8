794ded4af3580fb54ef8813371f38e5bc524ec44
Nadine273 <tomns@mail.ru>
2024-08-04 14:00:36 +0200

    Initial commit

---
+++ src/test/java/pro/sky/skyproempl/SkyProEmplApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproempl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProEmplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
